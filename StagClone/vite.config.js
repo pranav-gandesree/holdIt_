add2b14d94a99e1b1b31fe45c80e8da334a8baf7
pranav <pranav8267@gmail.com>
2024-07-22 21:38:48 +0530

    updated code

--- StagClone/vite.config.js
+++ StagClone/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [reactRefresh()],
-  // Add any additional Vite configurations here
+  plugins: [react()],
 });
--- backend/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async()=>{
-    return mongoose.connect(process.env.MONGO_URL)
-    .then(()=>{console.log('connected to databse')})
-    .catch((err)=>{console.log(err)});
-}
-
-module.exports = connectDB;
